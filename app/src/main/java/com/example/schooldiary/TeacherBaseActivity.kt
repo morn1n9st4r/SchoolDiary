b6e8c7d14e8e661a6eb82ce6408ec68e29752123
morn1n9st4r <tutgdeto2001@gmail.com>
2020-07-14 03:05:06 +0300

    teacher activity set to navigation drawer with fragments

--- app/src/main/java/com/example/schooldiary/TeacherBaseActivity.kt
+++ app/src/main/java/com/example/schooldiary/TeacherBaseActivity.kt
@@ -4,38 +4,71 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_drawer_teacher_main.*
 import kotlinx.android.synthetic.main.activity_teacher_base.*
 import java.util.*
 import kotlin.random.Random
 
-class TeacherBaseActivity : AppCompatActivity(), View.OnClickListener {
+class TeacherBaseActivity : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener  {
+
     private lateinit var auth: FirebaseAuth
+    private lateinit var drawer: DrawerLayout
     private val db = Firebase.firestore
     private var addedByEmail: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_teacher_base)
+        setContentView(R.layout.activity_drawer_teacher_main)
+        setSupportActionBar(toolbar)
         auth = Firebase.auth
-        btnAddStudent.setOnClickListener(this)
+        drawer = teacher_drawer_layout
+        var navigationView: NavigationView = nav_teacher_view
+        navigationView.setNavigationItemSelectedListener(this)
+        var toggle = ActionBarDrawerToggle(this, drawer, toolbar,
+                                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_teacher_container,
+                TeacherCalendarFragment()
+            ).commit()
+            navigationView.setCheckedItem(R.id.nav_teacher_calendar)
+        }
+    //    btnAddStudent.setOnClickListener(this)
+    //    btnCalendarTeacher.setOnClickListener(this)
+    }
+
+    override fun onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        tvTeacher.text = "TeacherID = ${auth.currentUser!!.uid} " +
-                "\n ${auth.currentUser!!.email}"
+/*        tvTeacher.text = "TeacherID = ${auth.currentUser!!.uid} " +
+                "\n ${auth.currentUser!!.email}"*/
         addedByEmail = auth.currentUser!!.email as String
     }
 
 
-    override fun onClick(p0: View?) {
+    override fun onClick(p0: View?) {/*
         when(p0) {
             btnAddStudent -> {
                 val studentEmail = etAddStudentEmail.text.toString()
@@ -76,6 +109,32 @@ class TeacherBaseActivity : AppCompatActivity(), View.OnClickListener {
                     }
 
             }
+            btnCalendarTeacher -> {
+                val calendarIntent = Intent(this, Calendar::class.java)
+                startActivityForResult(calendarIntent,0)
+            }
+        }*/
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_teacher_calendar -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_teacher_container,
+                                                                    TeacherCalendarFragment()).commit()
+            }
+            R.id.nav_teacher_students -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_teacher_container,
+                    TeacherStudentsFragment()).commit()
+            }
+            R.id.nav_teacher_settings -> {
+                Toast.makeText(this, "Settings",Toast.LENGTH_LONG).show()
+            }
+            R.id.nav_teacher_exit -> {
+                Toast.makeText(this, "Log Out",Toast.LENGTH_LONG).show()
+            }
         }
+
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
